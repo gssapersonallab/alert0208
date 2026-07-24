fdd575d0dbc3e2f828e5e05e8e5c5d4d447e8e93
gssapersonallab <sanjayavinash.2004@gmail.com>
2026-07-24 23:16:28 +0530

    Update DC_31_07.py

--- DC_31_07.py
+++ DC_31_07.py
@@ -343,7 +343,7 @@ def main():
                         human_date = humanize_date(s_date)
 
                         msg = (
-                            f"[{newly_unblocked_count} SEATS ]"
+                            f"PL ALLU CINEMAS {newly_unblocked_count} SEATS  \n"
                             f"{rows_str} rows unblocked for Spiderman Brand new day at Allu Cinemas .\n\n"
                             f"{human_date}, {s_time}"
                         )
